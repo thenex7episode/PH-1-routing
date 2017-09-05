f9d6721037996674be4a80a124275245bd864b76
Missy Beutler <missyjeanbeutler@users.noreply.github.com>
2017-09-05 16:18:19 -0700

    fixed file paths

--- demo/src/router.js
+++ demo/src/router.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
-import Home from './Home/Home.js';
-import About from './About/About.js';
-import FAQ from './About/FAQ/FAQ.js';
-import Company from './About/Company/Company.js';
-import People from './People/People.js';
-import Character from './Character/Character.js';
+import Home from './components/Home/Home.js';
+import About from './components/About/About.js';
+import FAQ from './components/About/FAQ/FAQ.js';
+import Company from './components/About/Company/Company.js';
+import People from './components/People/People.js';
+import Character from './components/Character/Character.js';
 
 export default (
     <div>
