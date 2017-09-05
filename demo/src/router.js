528aff5fd83c4f6e0400048a3a688491e3d9da38
Missy Beutler <missyjeanbeutler@users.noreply.github.com>
2017-09-05 16:16:55 -0700

    added router file

---
+++ demo/src/router.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
+import Home from './Home/Home.js';
+import About from './About/About.js';
+import FAQ from './About/FAQ/FAQ.js';
+import Company from './About/Company/Company.js';
+import People from './People/People.js';
+import Character from './Character/Character.js';
+
+export default (
+    <div>
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/people'>People</Link>
+      <Route exact path='/' component={Home}/>
+      <Route path='/about' render={() => {
+        return (
+          <About>
+            <Switch>
+              <Route path='/about/faq' component={FAQ}/>
+              <Route path='/about/company' component={Company}/>
+            </Switch>
+          </About>
+        )
+      }} />
+      <Switch>
+        <Route path='/people/:id' component={Character}/>
+        <Route path='/people' component={People}/>
+      </Switch>
+  </div>
+  )
