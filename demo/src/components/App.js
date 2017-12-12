a2107754bed5ae735bf355bf12cdb0e87e068d0f
Tyler Collier <tyler.collier+github@devmounta.in>
2017-12-12 02:47:24 -0700

    Ty's Step 3

--- demo/src/components/App.js
+++ demo/src/components/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
         This is App.js
-        <Route path="/" render={() => <div className="route">Home</div>} />
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/people">People</Link>
+        </nav>
+
+        <Route exact path="/" render={() => <div className="route">Home</div>} />
         <Route path="/about" render={() => <div className="route">About</div>} />
         <Route path="/people" render={() => <div className="route">People</div>} />
       </div>
