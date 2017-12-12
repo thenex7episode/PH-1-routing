5795ea288246fb09aa369d899d44537d6426b0ae
Tyler Collier <tyler.collier+github@devmounta.in>
2017-12-12 02:58:17 -0700

    Ty's Step 4

--- demo/src/components/About/About.js
+++ demo/src/components/About/About.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function About(props) {
   return (
     <div>
       <h1>This is the About component</h1>
-      <h3>FAQ</h3>
-      <h3>Company</h3>
+      <Link to="/about/faq">FAQ</Link>
+      <Link to="/about/company">Company</Link>
       {props.children}
     </div>
   )
--- demo/src/components/App.js
+++ demo/src/components/App.js
@@ -3,6 +3,8 @@ import { Route, Link } from 'react-router-dom';
 import Home from './Home/Home';
 import About from './About/About';
 import People from './People/People';
+import FAQ from './About/FAQ/FAQ';
+import Company from './About/Company/Company';
 
 class App extends Component {
   render() {
@@ -16,7 +18,12 @@ class App extends Component {
         </nav>
 
         <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
+        <Route path="/about" render={() => (
+          <About>
+            <Route path="/about/faq" component={FAQ} />
+            <Route path="/about/company" component={Company} />
+          </About>
+        )} />
         <Route path="/people" component={People} />
       </div>
     );
