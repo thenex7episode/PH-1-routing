7d4bec747ed3e0e1223c64eb26c058808b7ade33
Tyler Collier <tyler.collier+github@devmounta.in>
2017-12-12 02:50:35 -0700

    Should have been considered part of Step 2

--- demo/src/components/App.js
+++ demo/src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
+import Home from './Home/Home';
+import About from './About/About';
+import People from './People/People';
 
 class App extends Component {
   render() {
@@ -12,9 +15,9 @@ class App extends Component {
           <Link to="/people">People</Link>
         </nav>
 
-        <Route exact path="/" render={() => <div className="route">Home</div>} />
-        <Route path="/about" render={() => <div className="route">About</div>} />
-        <Route path="/people" render={() => <div className="route">People</div>} />
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/people" component={People} />
       </div>
     );
   }
