5a46c1869826c89028a6582be08d4b441c2bac8f
Tyler Collier <tyler.collier+github@devmounta.in>
2018-02-06 12:45:47 -0700

    Done

--- demo/src/components/About/About.js
+++ demo/src/components/About/About.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function About(props) {
   return (
     <div>
       <h1>This is the About component</h1>
-      <h3>FAQ</h3>
-      <h3>Company</h3>
+      <Link to="/about/faq"><h3>FAQ</h3></Link>
+      <Link to="/about/company"><h3>Company</h3></Link>
       {props.children}
     </div>
   )
--- demo/src/components/App.js
+++ demo/src/components/App.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import { Route, Switch, NavLink } from 'react-router-dom';
+import Home from './Home/Home';
+import About from './About/About';
+import People from './People/People';
+import FAQ from './About/FAQ/FAQ';
+import Company from './About/Company/Company';
+import Character from './Character/Character';
+
+const NotFound = () => <div>Not found 404</div>
 
 class App extends Component {
   render() {
     return (
       <div>
         This is App.js
+        <nav>
+          <NavLink exact activeClassName="active-link" to="/">Home</NavLink>
+          <NavLink activeClassName="active-link" to="/people">People</NavLink>
+          <NavLink activeClassName="active-link" to="/about">About</NavLink>
+        </nav>
+        <Switch>
+          <Route path="/about" render={() => {
+            return (
+              <About>
+                <Route path="/about/faq" component={FAQ} />
+                <Route path="/about/company" component={Company} />
+              </About>
+            )
+          }} />
+          <Route exact path="/people" component={People} />
+          <Route path="/people/:personId" component={Character} />
+          <Route exact path="/" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
       </div>
     );
   }
